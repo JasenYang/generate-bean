c9d902f4d1a77a3c8cd1b46e0688a07126468c66
JasenYang <2296685742@qq.com>
2019-03-22 03:01:43 +0800

    my first project is finished

---
+++ spec/generate-bean-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GenerateBeanView from '../lib/generate-bean-view';
+
+describe('GenerateBeanView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
